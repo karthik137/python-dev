c8add5294cb48d1db0ff67f4dce1fcb35a46b2f2
Mphasis <karthik.ganti@mphasis.com>
2019-03-20 14:14:06 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,28 @@
+20+32
+
+minute = 59
+print(minute/60)
+
+
+# floored integer division
+
+minute = 59
+result = minute//60
+
+print(result);
+
+# Expressions
+
+x = 17
+
+print(x+17)
+
+# NOTE: Always use PEMDAS rule for order of operations
+
+# print quotient
+quotient = 7 // 3
+print(quotient);
+
+# print remainder
+remainder = 7 % 3;
+print(remainder);
