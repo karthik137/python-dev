4d242fb28c26f5b60a2857e5ae9d12f0c9c49036
Mphasis <karthik.ganti@mphasis.com>
2019-03-20 14:05:44 +0530

    Variables and print

---
+++ print.py
@@ -0,0 +1,5 @@
+x = 6
+print (x)
+y = x * 7
+
+print(y)
---
+++ variables.py
@@ -0,0 +1,17 @@
+print("Printing integer");
+print(3);
+
+print("Printing type of string");
+print(type('Hello, World!'))
+
+print("Printing type of int");
+print(type(20));
+
+print("Printing type of float");
+print(type(3.2));
+
+
+print("printing int with commas");
+
+print(1,000,000)
+
